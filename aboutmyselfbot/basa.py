6b53ddce3970122031d5fd300591081abc92be1d
Илья <112815007+AndyLocks@users.noreply.github.com>
2022-10-17 13:14:28 +0200

    Update basa.py

--- aboutmyselfbot/basa.py
+++ aboutmyselfbot/basa.py
@@ -42,79 +42,79 @@ class Basa(metaclass=Singleton):
             "icon" : None
         }
         }})
-        with open("basa.json", "w") as file: file.write(json.dumps(basa, indent=4).replace("\'", "\""))
+        with open(self.url, "w") as file: file.write(json.dumps(basa, indent=4).replace("\'", "\""))
 
     def set_title(self, user: str, mean: str):
         with open(self.url, "r") as file: basa = json.load(file)
         basa[user]["body"]["title"] = mean
 
-        with open("basa.json", "w") as file: file.write(json.dumps(basa, indent=4).replace("\'", "\""))
+        with open(self.url, "w") as file: file.write(json.dumps(basa, indent=4).replace("\'", "\""))
 
     def set_description(self, user: str, mean: str):
         with open(self.url, "r") as file: basa = json.load(file)
         basa[user]["body"]["description"] = mean
 
-        with open("basa.json", "w") as file: file.write(json.dumps(basa, indent=4).replace("\'", "\""))
+        with open(self.url, "w") as file: file.write(json.dumps(basa, indent=4).replace("\'", "\""))
 
     def set_url(self, user: str, mean: str):
         with open(self.url, "r") as file: basa = json.load(file)
         basa[user]["body"]["url"] = mean
 
-        with open("basa.json", "w") as file: file.write(json.dumps(basa, indent=4).replace("\'", "\""))
+        with open(self.url, "w") as file: file.write(json.dumps(basa, indent=4).replace("\'", "\""))
 
     def set_color(self, user: str, mean: str):
         with open(self.url, "r") as file: basa = json.load(file)
         basa[user]["body"]["color"] = mean
 
-        with open("basa.json", "w") as file: file.write(json.dumps(basa, indent=4).replace("\'", "\""))
+        with open(self.url, "w") as file: file.write(json.dumps(basa, indent=4).replace("\'", "\""))
 
     def set_author(self, user: str, mean: str):
         with open(self.url, "r") as file: basa = json.load(file)
         basa[user]["author"]["title"] = mean
 
-        with open("basa.json", "w") as file: file.write(json.dumps(basa, indent=4).replace("\'", "\""))
+        with open(self.url, "w") as file: file.write(json.dumps(basa, indent=4).replace("\'", "\""))
     
     def set_author_url(self, user: str, mean: str):
         with open(self.url, "r") as file: basa = json.load(file)
         basa[user]["author"]["url"] = mean
 
-        with open("basa.json", "w") as file: file.write(json.dumps(basa, indent=4).replace("\'", "\""))
+        with open(self.url, "w") as file: file.write(json.dumps(basa, indent=4).replace("\'", "\""))
 
     def set_author_icon(self, user: str, mean: str):
         with open(self.url, "r") as file: basa = json.load(file)
         basa[user]["author"]["icon"] = mean
 
-        with open("basa.json", "w") as file: file.write(json.dumps(basa, indent=4).replace("\'", "\""))
+        with open(self.url, "w") as file: file.write(json.dumps(basa, indent=4).replace("\'", "\""))
 
     def set_image_thumbnail(self, user: str, mean: str):
         with open(self.url, "r") as file: basa = json.load(file)
         basa[user]["images"]["thumbnail"] = mean
 
-        with open("basa.json", "w") as file: file.write(json.dumps(basa, indent=4).replace("\'", "\""))
+        with open(self.url, "w") as file: file.write(json.dumps(basa, indent=4).replace("\'", "\""))
 
     def set_image(self, user: str, mean: str):
         with open(self.url, "r") as file: basa = json.load(file)
         basa[user]["images"]["url"] = mean
 
-        with open("basa.json", "w") as file: file.write(json.dumps(basa, indent=4).replace("\'", "\""))
+        with open(self.url, "w") as file: file.write(json.dumps(basa, indent=4).replace("\'", "\""))
 
     def set_footer(self, user: str, mean: str):
         with open(self.url, "r") as file: basa = json.load(file)
         basa[user]["footer"]["title"] = mean
 
-        with open("basa.json", "w") as file: file.write(json.dumps(basa, indent=4).replace("\'", "\""))
+        with open(self.url, "w") as file: file.write(json.dumps(basa, indent=4).replace("\'", "\""))
 
     def set_time(self, user: str, mean: str):
         with open(self.url, "r") as file: basa = json.load(file)
         basa[user]["footer"]["time"] = mean
 
-        with open("basa.json", "w") as file: file.write(json.dumps(basa, indent=4).replace("\'", "\""))
+        with open(self.url, "w") as file: file.write(json.dumps(basa, indent=4).replace("\'", "\""))
 
     def set_footer_icon(self, user: str, mean: str):
         with open(self.url, "r") as file: basa = json.load(file)
         basa[user]["footer"]["icon"] = mean
 
-        with open("basa.json", "w") as file: file.write(json.dumps(basa, indent=4).replace("\'", "\""))
+        with open(self.url, "w") as file: file.write(json.dumps(basa, indent=4).replace("\'", "\""))
 
 
 
@@ -169,4 +169,4 @@ class Basa(metaclass=Singleton):
     def clear_all(self, user: str):
         with open(self.url, "r") as file: basa = json.load(file)
         del basa[user]
-        with open("basa.json", "w") as file: file.write(json.dumps(basa, indent=4).replace("\'", "\""))
+        with open(self.url, "w") as file: file.write(json.dumps(basa, indent=4).replace("\'", "\""))
